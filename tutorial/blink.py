807e5996b05fd503f0538c7c24c4f230c29278c4
Jonathan Tsai <jontsai@jontsai.com>
2013-08-03 13:37:07 -0700

    cleaned up programs, added cleanup routine

--- tutorial/blink.py
+++ tutorial/blink.py
@@ -2,22 +2,47 @@ from time import sleep
 
 import RPi.GPIO as GPIO
 
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
+def main():
+    try:
+        cleanup()
+        setup()
+        driver_loop()
+    except KeyboardInterrupt:
+        print 'Got KeyboardInterrupt'
+    finally:
+        print 'Cleaning up...'
+        cleanup()
 
-while 1:
-     GPIO.output(13, False)
-     sleep(1)
-     GPIO.output(13, True)
-     sleep(1)
+def cleanup():
+    GPIO.cleanup()
 
-     GPIO.output(15, False)
-     sleep(1)
-     GPIO.output(15, True)
-     sleep(1)
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
 
-     GPIO.output(16, False)
-     sleep(1)
-     GPIO.output(16, True)
-     sleep(1)
+def driver_loop():
+    print 'Entering driver loop'
+
+    while True:
+        print 'Output 13'
+        GPIO.output(13, False)
+        sleep(1)
+        GPIO.output(13, True)
+        sleep(1)
+
+        print 'Output 15'
+        GPIO.output(15, False)
+        sleep(1)
+        GPIO.output(15, True)
+        sleep(1)
+
+        print 'Output 16'
+        GPIO.output(16, False)
+        sleep(1)
+        GPIO.output(16, True)
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
--- tutorial/inout.py
+++ tutorial/inout.py
@@ -1,18 +1,41 @@
 import RPi.GPIO as GPIO
 
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(11, GPIO.IN)
+def main():
+    try:
+        cleanup()
+        setup()
+        driver_loop()
+    except KeyboardInterrupt:
+        print 'Got KeyboardInterrupt'
+    finally:
+        print 'Cleaning up...'
+        cleanup()
 
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(12, GPIO.IN)
+def cleanup():
+    GPIO.cleanup()
 
-while 1:
-	if GPIO.input(11):
-		GPIO.output(13, False)
-	else:
-		GPIO.output(13, True)
-		
-	if GPIO.input(12):
-		GPIO.output(15, False)
-	else:
-		GPIO.output(15, True)
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    #GPIO.setwarnings(False)
+
+    GPIO.setup(11, GPIO.IN)
+    GPIO.setup(12, GPIO.IN)
+
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+
+def driver_loop():
+    print 'Entering driver loop'
+    while True:
+        if GPIO.input(11):
+            GPIO.output(13, False)
+        else:
+            GPIO.output(13, True)
+
+        if GPIO.input(12):
+            GPIO.output(15, False)
+        else:
+            GPIO.output(15, True)
+
+if __name__ == '__main__':
+    main()
